dc76a6725ba7800b514119550a7276b6c1d97991
Rajat <rajatuba9783@gmail.com>
2021-06-13 23:57:30 +0530

    Creating a Contact List

--- index.js
+++ index.js
@@ -6,9 +6,27 @@ const app=express();
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 
+var contactList=[
+    {
+        name : "Arpan",
+        phone : "111111111"
+    },
+    {
+        name : "Tony",
+        phone : "0132344654"
+    },
+    {
+        name : "Rajat",
+        phone : "78746746"
+    }
+]
+
 app.get('/',function(req,res){
     console.log(__dirname);
-    res.render('home',{title:"My Contact List"});
+    res.render('home',{
+        title:"My Contact List",
+        contact_list:contactList
+    });
 });
 
 app.listen(port,function(err){
